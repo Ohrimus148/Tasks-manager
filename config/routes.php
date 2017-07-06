ccdca8c9f9871362d1e75d30259fb29bfb59033d
Ohrimus148 <Ohrimus@yandex.ru>
2017-07-06 09:42:18 +0300

    php file which included all rotes of the projects

---
+++ config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'admin'=>'admin/index',  //actionIndex in NewsController
+   // 'user'=>'user/list', //actionList in UserController
+    'cabinet'=>'cabinet/index',
+    'user/register'=>'user/register', //actionRegister in UserController
+    'user/login'=>'user/login', //actionLogin in in UserController
+    '8'=>'task/category',
+    ''=>'task/index', //actionIndex in TaskController
+   // 'cabinet/page-([0-9]+)'=>'cabinet/index/$1'
+    
+);
+ 
+
